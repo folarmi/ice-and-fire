246b9503931ef4610fbc0be78360bf4268fcf6ca
folarmi <fola4ril@gmail.com>
2020-10-08 00:17:42 -0700

    Updates

---
+++ precache-manifest.1bdb0739195cc6848c88ea8c8d504f3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "155efc9cae4190c54ebd8250f077d738",
+    "url": "/folarmi.github.io/ice-and-fire/index.html"
+  },
+  {
+    "revision": "eb828070d87c1dc0779e",
+    "url": "/folarmi.github.io/ice-and-fire/static/css/main.a9d742c4.chunk.css"
+  },
+  {
+    "revision": "d6d087425ade7a7cb199",
+    "url": "/folarmi.github.io/ice-and-fire/static/js/2.8af843f0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/folarmi.github.io/ice-and-fire/static/js/2.8af843f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb828070d87c1dc0779e",
+    "url": "/folarmi.github.io/ice-and-fire/static/js/main.133167da.chunk.js"
+  },
+  {
+    "revision": "ffa8f0a43ff5c2bfa916",
+    "url": "/folarmi.github.io/ice-and-fire/static/js/runtime-main.ae485fba.js"
+  }
+]);
